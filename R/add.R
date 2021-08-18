9d26c9c2ef8a73ca91d828870857e9fe47eef4cc
JohnCoene <jcoenep@gmail.com>
2021-08-18 21:51:08 +0200

    added license

--- R/add.R
+++ R/add.R
@@ -3,6 +3,9 @@
 #' Add the `app.R` required to run on RStudio Connect,
 #' RStudio server, and shinyapps.io.
 #' 
+#' @importFrom cli cli_alert_success cli_alert_danger
+#' @importFrom usethis use_build_ignore
+#' 
 #' @export 
 add_app_file <- function(){
 	file <- pkg_file("run", "prod.R")
@@ -13,5 +16,6 @@ add_app_file <- function(){
 	}
 
 	cli_alert_success("Added {.file app.R}")
+	use_build_ignore("app.R")
 	copy_file(file, "app.R")
 }
