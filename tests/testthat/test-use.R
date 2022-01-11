623ec53c7f496111ad7ff881c4c8b0238cb25f23
John Coene <jcoenep@gmail.com>
2022-01-11 21:20:51 +0100

    fix: edit test use to skip cran

--- tests/testthat/test-use.R
+++ tests/testthat/test-use.R
@@ -55,21 +55,3 @@ test_that("use endpoints", {
   expect_message(use_endpoints_utils())
   expect_error(use_endpoints_utils())
 })
-
-test_that("use packer", {
-  wd <- getwd()
-  pkg <- create_tmp_package()
-  setwd(pkg)
-  on.exit({
-    setwd(wd)
-    delete_tmp_package(pkg)
-  })
-
-  scaffold()
-  expect_error(use_packer())
-  packer::scaffold_leprechaun(edit = FALSE)
-  expect_error(use_js_utils())
-  expect_message(use_packer())
-  expect_message(use_js_utils())
-  expect_null(use_packer())
-})
