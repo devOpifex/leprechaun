d9c15a3813325fbe9a40c443cc7cfb863a183d71
John Coene <jcoenep@gmail.com>
2022-01-15 21:36:59 +0100

    fix: use sass in roclet test to pass on cran

--- tests/testthat/test-roclet.R
+++ tests/testthat/test-roclet.R
@@ -1,7 +1,5 @@
 source("../fns.R")
 
-skip_on_cran()
-
 test_that("roclet", {
   wd <- getwd()
   pkg <- create_tmp_package()
@@ -12,8 +10,8 @@ test_that("roclet", {
   })
 
   scaffold()
-  packer::scaffold_leprechaun(edit = FALSE)
-  expect_error(use_js_utils())
-  expect_message(use_packer())
-  roxygen2::roxygenise(roclets = "leprechaun::build_roclet")
+  use_sass()
+  expect_message(
+    roxygen2::roxygenise(roclets = "leprechaun::build_roclet")
+  )
 })
